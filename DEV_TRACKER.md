d055e9f77fe72d8a72080f1f332ce86c7664ca09
ameysutavani <amey.sutavani@gmail.com>
2018-04-19 23:07:57 -0400

    Multithreading support

--- DEV_TRACKER.md
+++ DEV_TRACKER.md
@@ -1,6 +1,11 @@
 ### DEV TRACKER FOR LASER ROBOT SURGERY ###
-Current version: 0.0.1
-Date Modified: 2018-04-18 (YYYY-MM-DD)
+Current version: 0.0.2
+Date Modified: 2018-04-19 (YYYY-MM-DD)
+
+
+## Changes in version 0.0.1 ##
+	Changes:
+		Multithreading support for separate renderGUI thread and moveit thread.
 
 ## Changes in version 0.0.1 ##
 	Added files:
--- laserGUI.py
+++ laserGUI.py
@@ -1,14 +1,7 @@
 import numpy as np
 import cv2
-
-# mouse callback function
-def registerClick(event,x,y,flags,param):
-    global clicks, clicks_done
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        if clicks_done == False:
-            clicks = np.append(clicks, np.array([[x, y]]), axis = 0)
-        # print(clicks)
-        # print(clicks_done)
+import threading
+import time
 
 # Variable to store all the clicks
 clicks = np.zeros((1,2), np.uint8)
@@ -19,45 +12,85 @@ clicks_done = False
 # Final target points for the trajectory
 target_points = []
 
-# Define the frame to display everything
-cv2.namedWindow('frame')
 
-# Connect mouse interrupt
-cv2.setMouseCallback('frame',registerClick)
+# mouse callback function
+def registerClick(event,x,y,flags,param):
+	global clicks, clicks_done
+	if event == cv2.EVENT_LBUTTONDBLCLK:
+		if clicks_done == False:
+			clicks = np.append(clicks, np.array([[x, y]]), axis = 0)
+		# print(clicks)
+		# print(clicks_done)
 
-# Get camera object
-cap = cv2.VideoCapture(0)
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+def renderGUI(thread_name, delay):
+	# count = 0
+	# while count < 5:
+	# 	time.sleep(delay)
+	# 	count += 1
+	# 	# print "%s: %s" % (threadName, time.ctime(time.time()))
+	# 	print('{0} and {1}'.format(thread_name, time.ctime(time.time())))
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == True:
 
-        target_points = clicks[1:]
-        print(target_points)
-        for point in target_points:
-            # print(point)
-            if clicks_done == False:
-                cv2.circle(frame,tuple(point),5,(255,0,0),-1)
-            else:
-                cv2.circle(frame,tuple(point),5,(0,255,0),-1)
+	# Define the frame to display everything
+	cv2.namedWindow('frame')
 
-        cv2.imshow('frame',frame)
-        k = cv2.waitKey(1) & 0xFF
-        if k == ord('q'):
-            break
-        elif k == ord('d'):
-            clicks_done = True
+	# Connect mouse interrupt
+	cv2.setMouseCallback('frame',registerClick)
 
-        # write the flipped frame
-        # out.write(frame)
-    else:
-        break
+	# Get camera object
+	cap = cv2.VideoCapture(0)
 
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+	# Define the codec and create VideoWriter object
+	# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	# out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+	
+	global clicks, clicks_done, target_points
+	while(cap.isOpened()):
+		ret, frame = cap.read()
+		if ret == True:
+
+			target_points = clicks[1:]
+			# print(target_points)
+			for point in target_points:
+				# print(point)
+				if clicks_done == False:
+					cv2.circle(frame,tuple(point),5,(255,0,0),-1)
+				else:
+					cv2.circle(frame,tuple(point),5,(0,255,0),-1)
+
+			cv2.imshow('frame',frame)
+			k = cv2.waitKey(1) & 0xFF
+			if k == ord('q'):
+				break
+			elif k == ord('d'):
+				clicks_done = True
+
+			# write the flipped frame
+			# out.write(frame)
+		else:
+			break
+
+	# Release everything if job is finished
+	cap.release()
+	# out.release()
+	cv2.destroyAllWindows()
+
+def printInfo(delay):
+	global clicks, clicks_done
+	while 1:
+		time.sleep(delay)
+		# print "%s: %s" % (threadName, time.ctime(time.time()))
+		print('{0} Clicks_done: {1} and Click_points: {2}'.format(time.ctime(time.time()), clicks_done, target_points))
+
+
+# Create two threads as follows
+t1 = threading.Thread(target = renderGUI, args = ("Thread-1", 1,))
+t2 = threading.Thread(target = printInfo, args = (1,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
